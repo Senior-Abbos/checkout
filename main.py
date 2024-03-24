78354604c461e59d5674712c79c820a42d7c9493
abbos-khamidov <khamidov3538@gmail.com>
2024-03-24 15:33:51 +0500

    added searbon not working

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ import pandas as pd
 import panel as pn
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 
-df = pd.read_csv('')
+df = pd.read_csv('bank_dataset.csv')
 
-df.head(5)
+print(df.head(5))
+
+sns.heatmap(df)
