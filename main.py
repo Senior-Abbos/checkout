132c76f4834f5e1148cd5bdb0c74d75c40e0e059
abbos-khamidov <khamidov3538@gmail.com>
2024-03-24 15:35:35 +0500

    working version + data

--- main.py
+++ main.py
@@ -5,7 +5,15 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 df = pd.read_csv('bank_dataset.csv')
+data = pd.DataFrame({
+    'A': [1, 2, 3, 4],
+    'B': [5, 6, 7, 8],
+    'C': [9, 10, 11, 12]
+})
 
 print(df.head(5))
 
-sns.heatmap(df)
+sns.heatmap(data)
+
+plt.title('Heatmap example')
+plt.show()
